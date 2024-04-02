79f1aaee1685676a0e3dfd3869629aed4267e551
Romain Duquesne <127133643+rduquesne80@users.noreply.github.com>
2024-04-02 23:15:29 +0000

    top app bar & tabs instanciations

---
+++ main.js
@@ -0,0 +1,4 @@
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDC.MDCTopAppBar(topAppBarElement);
+
+const tabBar = new MDC.MDCTabBar(document.querySelector('.mdc-tab-bar'));
