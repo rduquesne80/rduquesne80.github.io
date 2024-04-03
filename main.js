dc918c4de2619e628b963216bdf16e0d0d3a8a22
Romain Duquesne <127133643+rduquesne80@users.noreply.github.com>
2024-04-03 00:43:19 +0000

    title style

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDC.MDCTopAppBar(topAppBarElement);
 
 const tabBar = new MDC.MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+let setActiveTab = (tabButton) => {
+    tabButton.addClass('mdc-tab--active');
+};
+const buttonsTab = document.querySelector('.mdc-tab');
+buttonsTab.forEach(button => {
+    button.addEventListener("click", (event) => {
+        buttonsTab.forEach(but => but.removeClass('mdc-tab--active'));
+        setActiveTab(button);
+    });
+});
