5fd9138047efab1b91ca6cc7e78952167bfb8edf
Romain Duquesne <127133643+rduquesne80@users.noreply.github.com>
2024-04-03 01:11:17 +0000

    active tab finish

--- main.js
+++ main.js
@@ -3,13 +3,3 @@ const topAppBar = new mdc.topAppBar.MDCTopAppBar(topAppBarElement);
 
 const tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
-let setActiveTab = (tabButton) => {
-    tabButton.addClass('mdc-tab--active');
-};
-const buttonsTab = Array.from(document.getElementsByClassName('.mdc-tab'));
-buttonsTab.forEach(button => {
-    button.addEventListener("click", (event) => {
-        buttonsTab.forEach(but => but.removeClass('mdc-tab--active'));
-        setActiveTab(button);
-    });
-});
