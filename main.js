f3e6656f65a9a3426cfea1786ab22fc466d37b9e
Romain Duquesne <127133643+rduquesne80@users.noreply.github.com>
2024-04-03 01:01:30 +0000

    replace queryselect by getele by class

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
 let setActiveTab = (tabButton) => {
     tabButton.addClass('mdc-tab--active');
 };
-const buttonsTab = document.querySelector('.mdc-tab');
+const buttonsTab = document.getElementsByClassName('.mdc-tab');
 console.log(buttonsTab);
 buttonsTab.forEach(button => {
     button.addEventListener("click", (event) => {
