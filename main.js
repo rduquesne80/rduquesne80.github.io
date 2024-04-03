ffabb39a4ed58b7c750691359b557021dcca5b7f
Romain Duquesne <rduquesne80@gmail.com>
2024-04-03 19:36:49 +0200

    data

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const topAppBar = new mdc.topAppBar.MDCTopAppBar(topAppBarElement);
 
 const tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
+fetch('portrait.html')
+  .then(response=> response.text())
+  .then(text=> document.getElementById('container').innerHTML = text);
+
+let loadTab = (tabName) => {
+    fetch(tabName + '.html')
+        .then(response=> response.text())
+        .then(text=> document.getElementById('container').innerHTML = text);
+};
